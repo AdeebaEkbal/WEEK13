778bddbec9b3ef111166c8e99e6ab88561f83a4a
Adeeba Ekbal <adeebaekbal@gmail.com>
2025-11-09 10:41:00 +0530

    question1

---
+++ ques1_13/Ques1_13.java
@@ -0,0 +1,9 @@
+package ques1_13;
+import java.io.*;
+public class Ques1_13 
+{
+    public static void main(String[] args) throws Exception
+    {
+        FileOutputStream fo = new FileOutputStream("a.txt");
+    }
+}
